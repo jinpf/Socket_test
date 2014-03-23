8eb22746910fea01d58e51383e62a5a0e2411bd2
jinpf <jpflcj@sina.com>
2014-03-24 00:20:03 +0800

    file transfer
    file transfer using socket

---
+++ src/file/FClient.java
@@ -0,0 +1,86 @@
+package file;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * file transfer client with no GUI
+ * @author jinpf
+ * 
+ */
+public class FClient {
+
+	public static void main(String[] args) {
+		Socket client=null;
+		DataOutputStream dout=null;
+		FileInputStream fi=null;
+		byte[] sendBytes=null;
+		int len=0;	//read file data length at one time
+		Boolean Sflag=false; //send successful or not 
+		
+		try {
+			
+			File f=null;
+			Boolean tag=false;
+			do{
+				if (tag){
+					System.out.println("please input legal file path!!!");
+				}
+				System.out.print("Please input file Path:");
+				BufferedReader input = null ;	// receive from keyboard
+				input = new BufferedReader(new InputStreamReader(System.in)) ;
+				String str = input.readLine() ;		// receive data from keyboard
+				f=new File(str);
+				tag=true;
+			}while(!f.exists());
+			
+			//send file
+			try{
+				client=new Socket("localhost",8888);
+				dout=new DataOutputStream(client.getOutputStream());
+				fi=new FileInputStream(f);
+				sendBytes=new byte[1024];
+				long L=f.length();
+				long sumL=0;
+				
+				//send file name
+				dout.writeUTF(f.getName());
+				//send file
+				while( (len=fi.read(sendBytes, 0, sendBytes.length))>0 ){
+					dout.write(sendBytes, 0, len);
+					dout.flush();
+					sumL+=len;
+					System.out.println("send "+(sumL/L*100)+"%");
+				}
+				if(sumL==L)
+					Sflag=true;
+			}catch(Exception e){
+				System.out.println("client send failure...");
+				Sflag=false;
+				e.printStackTrace();
+			}finally{
+				if(fi!=null){
+					fi.close();
+				}
+				if(dout!=null){
+					dout.close();
+				}
+				if(client!=null){
+					client.close();
+				}
+			}
+	
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+		System.out.println("File send "+(Sflag?"success!":"failure..."));
+	}
+
+}
---
+++ src/file/FServer.java
@@ -0,0 +1,105 @@
+package file;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * file transfer server with no GUI
+ * @author jinpf
+ *
+ */
+public class FServer {
+
+	public static void main(String[] args) {
+		try{
+			final ServerSocket server=new ServerSocket(8888);
+			System.out.println("Server start,wait for Client...");
+			//using multiThreading to handle multiClient
+			Thread t=new Thread(
+					
+					new Runnable(){
+						public void run(){
+							while(true){
+								try{
+									Socket client=server.accept();
+									Receivefile(client);
+								}catch(Exception e){
+									System.out.println("Server exception!");
+									e.printStackTrace();
+								}	
+							}				
+						}						
+					}
+					
+					);
+			
+			//start thread
+			t.run();
+			server.close();
+		}catch(Exception e){
+			e.printStackTrace();		
+		}
+
+	}
+	
+	/**
+	 * receive file from client,file path at D:/temp/
+	 * @param client
+	 * @author jinpf
+	 */
+	protected static void Receivefile(Socket client) {
+		DataInputStream di=null;
+		FileOutputStream fo=null;
+		byte[] inputByte=null;
+		int len=0;	//receive data length at one time
+		
+		try{
+			
+			try{
+				di=new DataInputStream(client.getInputStream());
+				
+				String ID=client.getInetAddress()+":"+client.getPort();
+				String filename=di.readUTF();
+				System.out.println("start receive "+ID+"--"+filename);
+				
+				//set file path
+				String filepath="D:"+File.separator+"temp";
+				File f=new File(filepath);
+				if(!f.exists()){
+					f.mkdir();
+				}
+				//if file exist rename the file
+				f=new File(filepath+File.separator+filename);
+				while(f.exists()){
+					f=new File(f.getParent()+File.separator+f.getName()+".rec");
+				}
+				
+				//receive file
+				fo=new FileOutputStream(f);
+				inputByte=new byte[1024];
+				while( (len=di.read(inputByte, 0, inputByte.length)) >0){
+					fo.write(inputByte,0,len);
+					fo.flush();
+				}
+				System.out.println(ID+"--"+filename+" receive complete!");	
+			}finally{
+				if(fo!=null){
+					fo.close();
+				}
+				if(di!=null){
+					di.close();
+				}
+				if(client!=null){
+					client.close();
+				}
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
